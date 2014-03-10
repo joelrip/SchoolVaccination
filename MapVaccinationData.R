a1ea970e31632c4169e5e1581936dee137ec6c96
joelrip <joelrip@gmail.com>
2014-03-09 22:44:27 -0700

    3/9/14

--- MapVaccinationData.R
+++ MapVaccinationData.R
@@ -14,7 +14,7 @@ ChildCareData_spdf <- SpatialPointsDataFrame(CCcoord, ChildCareDataSub, proj4str
 ChildCareData_spdf <- spTransform(ChildCareData_spdf, CRS=CRS(proj4string(SACOGOutline)))
 
 ##Plot points, color-coded
-require(RColorBrewer)
+#require(RColorBrewer)
 require(classInt)
 CCBreaks <- classIntervals(ChildCareData_spdf$PBEPctCalc, n=3, style="fixed", fixedBreaks=c(0,0.03,0.1,1))
 #pal <- brewer.pal(3, "RdYlGn")
@@ -23,5 +23,19 @@ CCColors <- findColours(CCBreaks, pal=rev(pal))
 plot(ChildCareData_spdf, pch=19, cex=0.5, col=CCColors, axes=T, xlim=c(6675000,6840000), ylim=c(1900000,2075000))
 
 ##Put in Google Maps
+##Prettify output
+CCVaccination <- ChildCareData_spdf
+CCVaccination$PBEPctCalc <- round(CCVaccination$PBEPctCalc, 2)
+CCVaccination <- CCVaccination[,c(-4,-5,-8,-10,-12,-14,-16,-18,-20,-22,-24,-26,-27)]
+CCVaccination <- CCVaccination[,c(1:3,17:23,4:15)]
+#require(scales)
+#CCVaccination$PBEPctCalc <- percent(CCVaccination$PBEPctCalc)
+names(CCVaccination) <- c("Facility Number","County","Type","License Status","Name","Address",
+                          "City","State","ZIP","Phone","Total Students","Up To Date","Conditional",
+                          "Permanent Medical Exemptions","Personal Belief Exemptions","DTP","Polio",
+                          "MMR","HIB","HepB","Varicella","Pct Personal Belief Exemptions")
+#Plot
 require(plotGoogleMaps)
-plotGoogleMaps(ChildCareData_spdf, zcol="PBEPctCalc", colPalette=c("#91CF60","#F9F030","#FC5050"), at=c(0,0.03,0.1,1), layerName="Child Care Facilities")
+ic <- iconlabels(CCVaccination$Pct.Personal.Belief.Exemptions)
+plotGoogleMaps(CCVaccination, zcol="Pct Personal Belief Exemptions", mapTypeId="ROADMAP", colPalette=c("#91CF60","#F9F030","#FC5050"), at=c(0,0.03,0.1,1), layerName="Child Care Facilities 2012-13")
+#bubbleGoogleMaps(ChildCareData_spdf, zcol="PBEPctCalc", mapTypeId="ROADMAP", colPalette=c("#91CF60","#F9F030","#FC5050"), layerName="Child Care Facilities")
